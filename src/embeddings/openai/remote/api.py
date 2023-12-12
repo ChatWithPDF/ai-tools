a28168b71e45295ede6d743fd5fadbb6889b8a1d
Gautam <102940730+GautamR-Samagra@users.noreply.github.com>
2023-12-12 09:13:41 +0530

    Update api.py

--- src/embeddings/openai/remote/api.py
+++ src/embeddings/openai/remote/api.py
@@ -1,20 +1,42 @@
 from model import Model
 from request import ModelRequest
-from quart import Quart, request
+from quart import Quart, request, send_file
 import aiohttp
+import pandas as pd
+import io
 
-#from fastapi import FastAPI, Body
 app = Quart(__name__)
-#app.client = aiohttp.ClientSession()
-#app = FastAPI()
 
 @app.before_serving
 async def startup():
     app.client = aiohttp.ClientSession()
 
 @app.route('/', methods=['POST'])
-async def translate():
+async def embed():
+    files = await request.files
+    uploaded_file = files.get('file')
     data = await request.get_json()
-    req = ModelRequest(**data)
     model = Model(app)
-    return await model.inference(req)
+
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file.stream)
+        if df.empty or df['content'].isnull().any():
+            return {'error': 'There are nonzero null rows'}, 400
+
+        req = ModelRequest(df=df)
+        response = await model.inference(req)
+
+        if response == 'There are nonzero null rows':
+            return {'error': response}, 400
+
+        df = pd.read_csv(io.StringIO(response))
+        df.to_csv('output.csv', index=False)
+        return await send_file('output.csv', mimetype='text/csv', as_attachment=True, attachment_filename='output.csv')
+    else:
+        req = ModelRequest(**data)
+        response = await model.inference(req)
+
+        if response == 'There are nonzero null rows':
+            return {'error': response}, 400
+
+        return response
